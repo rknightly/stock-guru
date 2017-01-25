bf17278ded3be32875013b3a4e99df0e0ba7205f
rknightly <reknightly@gmail.com>
2017-01-24 18:51:38 -0600

    Change to google finance

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-var googleStocks = require('google-stocks');
+var googleFinance = require('google-finance');
 
-googleStocks(['APPL'], function(error, data) {
-    console.log(data);
+var SYMBOL = "NASDAQ:AAPL";
+var START_DATE = "2017-01-01";
+var END_DATE = "2017-01-24";
+
+googleFinance.historical({
+    symbol: SYMBOL,
+    from: START_DATE,
+    to: END_DATE
+}, function (err, quotes) {
+    if (err) {
+        console.log("An error occured");
+    } else {
+        console.log(quotes);
+    }
 });
