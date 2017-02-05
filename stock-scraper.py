32aa0bfc4a80798074c5e42006055a02eb9c7af7
Ryan Knightly <reknightly@gmail.com>
2017-02-04 21:53:26 -0600

    Add analysis to stocks after gathered and fixed error

--- stock-scraper.py
+++ stock-scraper.py
@@ -9,24 +9,29 @@ class StockData:
         self.industry = industry
 
         self.estimated_change_percent = 0
-        self.data_found = false
+        self.recommended_action = ""
 
-    def find_estimated_change_percent(self):
-        # Get the soup of the website
+        self.data_found = False
+
+        self.soup  = BeautifulSoup()
+
+    def get_soup(self):
         url_address = "http://money.cnn.com/quote/forecast/forecast.html?symb=%s" % self.ticker
         r = urllib.request.urlopen(url_address).read()
 
-        soup = BeautifulSoup(r, "lxml")
+        self.soup = BeautifulSoup(r, "html.parser")
 
+    def find_estimated_change_percent(self):
         # search the soup for the forecast
-        analysis = soup.find_all("div", id="wsod_forecasts")
+        analysis = self.soup.find_all("div", id="wsod_forecasts")
 
         # If bad ticker given, print error and skip rest
         if len(analysis) == 0:
             print("No data found for:", self.ticker, self.name)
+            self.data_found = False
             return
         else:
-            self.data_found = true
+            self.data_found = True
 
         analysis = analysis[0]
 
@@ -39,13 +44,25 @@ class StockData:
         if len(projected_change) == 0:
             projected_change = analysis_text.find_all("span", class_="posData")
 
+        # If can't find projected change, forget it
+        if len(projected_change) == 0:
+            print("No data found for:", self.ticker, self.name)
+            self.data_found = False
+            return
+
         self.estimated_change_percent = float(projected_change[0].text[:-1])
+
+    def find_recommended_action(self):
+        # TODO: find section of page that states buy/sell/hold recommendation
+        pass
 
     def print_report(self):
         print(self.ticker, self.name)
         print("Estimated Change: %.1f%%" % self.estimated_change_percent)
         print()
 
+    def print_one_line_report(self):
+        print(self.ticker, self.name + ":", str(self.estimated_change_percent) + "%")
 
 # TODO: add analyst count report and estimate spread report
 
@@ -61,19 +78,40 @@ class StockSearcher:
             reader = csv.reader(f)
             for row in reader:
                 file_rows.append(row)
+        print(file_rows)
 
         for stock_info in file_rows:
             ticker, name, industry = stock_info
             stock_data = StockData(ticker, name, industry)
 
-            if stock_data.data_found:
-                self.stock_list.append(stock_data)
+            self.stock_list.append(stock_data)
 
     def get_data_of_stocks(self):
-        for stock in self.stock_list:
+        indeces_to_remove = []
+        # Request data for each stock
+        for index, stock in enumerate(self.stock_list):
+            stock.get_soup()
             stock.find_estimated_change_percent()
+            if not stock.data_found:
+                indeces_to_remove.append(index)
             stock.print_report()
+
+        # remove stocks with no results
+        for index in indeces_to_remove[::-1]:
+            self.stock_list.pop(index)
+
+    def print_highest_projected_stocks(self):
+        print("=========== REPORT ============")
+
+        # Sort the stocks by their estimated change percent, highest to lowest
+        sorted_stocks = sorted(self.stock_list,
+                               key=lambda stock: stock.estimated_change_percent,
+                               reverse=True)
+        for stock in sorted_stocks[:50]:
+            stock.print_one_line_report()
+
 
 searcher = StockSearcher(file_name = 'constituents.csv')
 searcher.get_stocks_from_file()
 searcher.get_data_of_stocks()
+searcher.print_highest_projected_stocks()
