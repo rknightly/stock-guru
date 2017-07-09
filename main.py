902da0f21307251c973e62f06222981cf6713b0e
rknightly <reknightly@gmail.com>
2017-07-09 11:23:30 -0400

    Changing default searchspace to combined

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from StockGuru.StockSearcher import StockSearcher
 
 def main():
-    searcher = StockSearcher(file_name='shortened-cap.csv')
+    searcher = StockSearcher(file_name='combined-cap.csv')
     searcher.run()
 
 if __name__ == "__main__":
